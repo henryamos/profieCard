b5cf33495faa86d2a4eea7e8808f849b7bc9097b
Henry Amos <amoshenry95@gmail.com>
2024-04-26 12:40:00 +0000

    PWA testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,95 @@
 import React from 'react'
-import ToggleButton from './ToggleButton'
-// import  Header  from './components/Header'
-// import  Footer  from './components/Footer'
-// import Lessons from './Lessons'
-// import Welcome from './Welcome'
-// import Clock from './Clock'
+import profilePic from "./assets/ben.jpg"
+const Skills=[
+  {
+  skill: "HTML + CSS ",
+  level:"Advanced",
+  color:"#2662EA"
+},
+{
+  skill: "Tailwind CSS",
+  level:"Advanced",
+  color:"#EFD81D"
+},
+{
+  skill: "Javascript",
+  level:"Intermediate",
+  color:"#2662EA"
+},
+{
+  skill: "Git + GitHub",
+  level:"Intermediate",
+  color:"#2662EA"
+},
+{
+  skill: "React JS",
+  level:"Intermediate",
+  color:"#2662EA"
+},
+{
+  skill: "Next JS",
+  level:"Beginner",
+  color:"#2662EA"
+},
+]
 
 const App = () => {
   return (
-    <div>
-     {/* <Lessons/> */}
-     {/* <Welcome name="Henry" lastName="Amos"/> */}
-     {/* <Clock date={new Date()}/> */}
-     <ToggleButton/>
+    <div className='card'>
+      <Avatar  image ={profilePic}/>
+      <div className="data">
+        <Intro/>
+        <SkillSet/>
+      </div>
+    
     </div>
+   
+   
   )
 }
+const Avatar = ({image})=>{
+return(
+<img  className ="avatar" 
+src={image} alt="" />
+)
+}
+const Intro = ()=>{
+return(
+   <div>
+      <h1>Bnejamin Henry Amos</h1>
+      <p> Full-stack web developer and teacher at Udemy. When not coding or
+        preparing a course, I like to play board games, to cook (and eat), or to
+        just enjoy the Portuguese sun at the beach.</p>
+   </div>
+)
+}
+const SkillSet=()=>{
+return (
+  <div className='skill-list'>
+    {Skills.map((skills,index)=>(
+      <Skill 
+      key={index} 
+      skill={skills.skill} 
+      level={skills.level}
+      color={skills.color}
+      
+      />
+    ))}
+     
+ 
+  </div>
+)
+}
+const Skill=({skill,level,color})=>{
+  return(
+    <div className="skill" style={{ backgroundColor:color }}>
+      <span>{skill}</span>
+         <span>{level === "Beginner" && "👶"  }</span>
+         <span>{level === "Intermediate" && "🤝" }</span>
+         <span>{level === "Advanced" && "🖐"  }</span>
 
+    </div>
+  )
+
+}
 export default App
